42ca973165184216d134a415bc0596a4009d31db
gagan2908 <virkgagan1322@gmail.com>
2026-08-02 22:11:34 +0530

    Thirty seventh commit

---
+++ Day37.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int pq[MAX];
+int size = 0;
+
+// Insert element in sorted order
+void insert(int x) {
+    int i = size - 1;
+
+    while (i >= 0 && pq[i] > x) {
+        pq[i + 1] = pq[i];
+        i--;
+    }
+
+    pq[i + 1] = x;
+    size++;
+}
+
+// Delete highest priority element
+void deleteElement() {
+    if (size == 0) {
+        printf("-1\n");
+        return;
+    }
+
+    printf("%d\n", pq[0]);
+
+    for (int i = 1; i < size; i++) {
+        pq[i - 1] = pq[i];
+    }
+
+    size--;
+}
+
+// Peek highest priority element
+void peek() {
+    if (size == 0) {
+        printf("-1\n");
+        return;
+    }
+
+    printf("%d\n", pq[0]);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    char op[10];
+    int x;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%s", op);
+
+        if (strcmp(op, "insert") == 0) {
+            scanf("%d", &x);
+            insert(x);
+        }
+        else if (strcmp(op, "delete") == 0) {
+            deleteElement();
+        }
+        else if (strcmp(op, "peek") == 0) {
+            peek();
+        }
+    }
+
+    return 0;
+}
