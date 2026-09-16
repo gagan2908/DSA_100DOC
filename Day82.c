ee85aada58f02bc1480f322cdc5ff7218259e67b
gagan2908 <virkgagan1322@gmail.com>
2026-09-16 19:05:18 +0530

    Eighty Second commit

---
+++ Day82.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int lowerBound(int arr[], int n, int x) {
+    int low = 0, high = n;
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] >= x) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+}
+
+int upperBound(int arr[], int n, int x) {
+    int low = 0, high = n;
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] > x) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+}
+
+int main() {
+    int n, x;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &x);
+
+    int lb = lowerBound(arr, n, x);
+    int ub = upperBound(arr, n, x);
+
+    printf("%d %d\n", lb, ub);
+
+    return 0;
+}
