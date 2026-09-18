9d6461f477cb306ebc0c9c9f037f94f57ebd0896
gagan2908 <virkgagan1322@gmail.com>
2026-09-18 22:02:31 +0530

    Eighty Fourth commit

---
+++ Day84.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Insertion Sort
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Shift elements greater than key to the right
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // Insert key at correct position
+        arr[j + 1] = key;
+    }
+
+    // Print sorted array
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+
+        if (i < n - 1)
+            printf(" ");
+    }
+
+    return 0;
+}
